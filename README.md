d1237d56dd3c9ebd25793c041e85065860071526
xandone <765478955@qq.com>
2022-10-26 15:40:35 +0800

    init

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 
 # 延伸阅读
 
+
+
+启动:
+<br/>
+nohup java -jar tinyshare-1.0.0.jar >server.log 2 >&1 &
+不挂起运行服务
+
+关闭:
+<br/>
+ps -ef|grep tinyshare-1.0.0.jar 查看端口
+kill -9 pid端口
--- src/main/java/com/xandone/tinyshare/bean/AssetsBean.java
+++ src/main/java/com/xandone/tinyshare/bean/AssetsBean.java
@@ -96,7 +96,7 @@ public class AssetsBean {
         this.assetCode = assetCode;
     }
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     public Date getPostTime() {
         return postTime;
     }
